1cb5ad8bf58d57dd8cf1a262acf03b8278de9034
btx <zdenek@softic.cz>
2013-05-10 15:09:40 +0200

    API created.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import json
 import jinja2
 import os
 import webapp2
 
-from google.appengine.api import users
+from google.appengine.api import search, users
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),
+                                                'templates')))
+_INDEX_NAME = "annotations"
 
 
 class HelloHandler(webapp2.RequestHandler):
@@ -18,22 +21,94 @@ class HelloHandler(webapp2.RequestHandler):
         else:
             url = users.create_login_url(self.request.uri)
 
-        template_values = {
+        context = {
             'users': users,
             'user': user,
             'url': url,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+        self.response.write(template.render(context))
 
 
-class StoreHandler(webapp2.RedirectHandler):
+class AnnotationsHandler(webapp2.RequestHandler):
+    def _prepare(self, results):
+        data = []
+
+        for result in results:
+            item = {
+                'id': result.doc_id,
+            }
+            for field in result.fields:
+                if field.name == 'shapes':
+                    item[field.name] = json.loads(field.value)
+                else:
+                    item[field.name] = field.value
+            data.append(item)
+
+        return data
+
+    def get(self, annotation_id=None):
+        query = ''
+
+        query_options = search.QueryOptions(
+            limit=1000,
+            # returned_fields=['doc_id', 'text', 'src'],
+            # snippeted_fields=['text'],
+        )
+        query_obj = search.Query(query_string=query, options=query_options)
+        results = search.Index(name=_INDEX_NAME).search(query=query_obj)
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(self._prepare(results)))
+
+    # TODO: handle errors properly
     def post(self):
-        self.response.write(self.request)
+        data = self.request.get('annotation')
+
+        annotation = None
+
+        try:
+            annotation = json.loads(data)
+        except ValueError:
+            # invalid input
+            pass
+        else:
+            doc = search.Document(
+                doc_id=annotation.get('id', None),
+                fields=[
+                    search.TextField(name='text', value=annotation['text']),
+                    search.TextField(name='src', value=annotation['src']),
+                    search.TextField(name='context',
+                                     value=annotation['context']),
+                    search.TextField(name='shapes',
+                                     value=json.dumps(annotation['shapes'])),
+                ]
+            )
+
+            try:
+                # raise search.Error()
+                result = search.Index(name=_INDEX_NAME).put(doc)
+            except search.Error:
+                # logging.exception('Put failed')
+                pass
+            else:
+                annotation['id'] = result[0].id
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(annotation))
+
+    def delete(self, annotation_id):
+        index = search.Index(name=_INDEX_NAME)
+        index.delete(annotation_id)
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps('{}'))
 
 
 app = webapp2.WSGIApplication([
-    ('/', HelloHandler),
-    ('/store', StoreHandler),
+    webapp2.Route(r'/', HelloHandler, name='home'),
+    webapp2.Route(r'/annotations', AnnotationsHandler, name='annotation_list'),
+    webapp2.Route(r'/annotations/<annotation_id:[-\w]+>', AnnotationsHandler,
+                  name='annotation'),
 ], debug=True)
--- static/main.js
+++ static/main.js
@@ -1,27 +1,79 @@
-goog.require('goog.net.XmlHttp');
+goog.provide('kt_annotations.main');
+
+goog.require('goog.proto.Serializer');
+goog.require('goog.Uri');
+goog.require('goog.net.XhrIo');
 
 
-anno.addHandler('onAnnotationCreated', function (annotation) {
-    annotation.id = Math.random().toString(36).substring(7);
-    console.log('created', annotation);
-});
+kt_annotations.main = function() {
 
-anno.addHandler('onAnnotationUpdated', function (annotation) {
-    console.log('updated', annotation);
-});
-
-anno.addHandler('onAnnotationRemoved', function (annotation) {
-    console.log('removed', annotation);
-});
-
-var getDataAndAlertIt = function (url) {
-    var xhr = goog.net.XmlHttp();
-    var isAsynchronous = true;
-    xhr.open('GET', url, isAsynchronous);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == goog.net.XmlHttp.ReadyState.COMPLETE) {
-            alert('This alert will display second: ' + xhr.responseText);
-        }
-    };
-    xhr.send(null);
 };
+
+kt_annotations.main.load = function() {
+    var xhr = new goog.net.XhrIo();
+
+    goog.events.listen(xhr, goog.net.EventType.SUCCESS, function(e) {
+        var annotations = e.target.getResponseJson();
+
+        goog.array.forEach(annotations, function(annotation) {
+            anno.addAnnotation(annotation);
+        })
+    });
+
+    xhr.send('/annotations', 'GET', qd);
+};
+
+kt_annotations.main.store = function(annotation, callback) {
+    var serializer = new goog.proto.Serializer(null);
+    var qd = new goog.Uri.QueryData();
+    qd.add('annotation', serializer.serialize(annotation));
+
+    callback = callback || function() {};
+
+    var xhr = new goog.net.XhrIo();
+
+    goog.events.listen(xhr, goog.net.EventType.SUCCESS, function (e) {
+        var obj = e.target.getResponseJson();
+        annotation.id = obj.id;
+
+        callback.apply(this, [annotation]);
+    });
+
+    xhr.send('/annotations', 'POST', qd);
+};
+
+kt_annotations.main.remove = function(annotation, callback) {
+    callback = callback || function() {};
+
+    var xhr = new goog.net.XhrIo();
+    goog.events.listen(xhr, goog.net.EventType.SUCCESS, callback);
+    xhr.send('/annotations/' +  annotation.id, 'DELETE');
+};
+
+kt_annotations.main.start = function() {
+    anno.addHandler('onAnnotationCreated', function (annotation) {
+        kt_annotations.main.store(annotation, function() {
+            console.log('created', annotation);
+        });
+    });
+
+    anno.addHandler('onAnnotationUpdated', function (annotation) {
+        kt_annotations.main.store(annotation, function() {
+            console.log('updated', annotation);
+        });
+    });
+
+    anno.addHandler('onAnnotationRemoved', function (annotation) {
+        kt_annotations.main.remove(annotation, function() {
+            console.log('removed', annotation);
+        });
+    });
+
+//    anno.makeAnnotatable(document.getElementById('img_1'));
+    setTimeout(function() {
+        kt_annotations.main.load();
+    }, 500);
+};
+
+goog.exportSymbol('main', kt_annotations.main);
+goog.exportProperty(kt_annotations.main, 'start', kt_annotations.main.start);
